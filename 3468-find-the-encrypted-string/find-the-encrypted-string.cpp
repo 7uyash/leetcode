3c7db1ff35c9d35c4c20f33352f1bf96a3f229d8
Suyash sahu <142486268+7uyash@users.noreply.github.com>
2024-07-07 20:40:19 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (50.00%) - LeetSync

---
+++ 3468-find-the-encrypted-string/find-the-encrypted-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string getEncryptedString(string s, int k) {
+        int n = s.length();
+        string ans=s;
+        for(int i=0;i<n; i++){
+            ans[i]=s[(i+k)%n];
+            
+        }
+        return ans;
+        
+    }
+};
