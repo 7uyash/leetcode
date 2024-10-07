ed52caf55b70c2f5b2826670ceb856af654ddf41
Suyash sahu <142486268+7uyash@users.noreply.github.com>
2024-10-07 21:22:58 +0530

    Time: 8 ms (40.94%) | Memory: 25.1 MB (89.34%) - LeetSync

---
+++ 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.cpp
@@ -0,0 +1,15 @@
+class Solution {  
+public:  
+    double minimumAverage(std::vector<int>& nums) {  
+        std::sort(nums.begin(), nums.end());  
+        int n = nums.size();  
+        double minAvg = std::numeric_limits<double>::max();  
+
+        for (int i = 0; i < n / 2; ++i) {  
+            double average = (nums[i] + nums[n - 1 - i]) / 2.0;  
+            minAvg = std::min(minAvg, average);  
+        }  
+
+        return minAvg;  
+    }  
+};
