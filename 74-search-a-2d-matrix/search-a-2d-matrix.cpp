cf9162fd4d8273af052c81804f56dfa26d44f4e9
Suyash sahu <142486268+7uyash@users.noreply.github.com>
2025-10-07 10:19:38 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (7.32%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        if (matrix.empty() || matrix[0].empty()) {
+            return false;
+        }
+        int n = matrix.size();   
+        int m = matrix[0].size(); 
+        int low = 0, high = n - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (matrix[mid][0] == target) {
+                return true;
+            } else if (matrix[mid][0] < target) {
+                low = mid + 1; 
+            } else {
+                high = mid - 1; 
+            }
+        }
+
+        if (low == 0) return false; 
+        
+        int row = low - 1;
+        low = 0;
+        high = m - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (matrix[row][mid] == target) {
+                return true; 
+            } else if (matrix[row][mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return false; 
+    }
+};
